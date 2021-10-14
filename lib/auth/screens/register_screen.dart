fc265af6b011c7e404c99387451e661f2ee3f6db
loicgeek <loic.ngou98@gmail.com>
2021-10-14 08:07:31 +0100

    Password validation ready

--- lib/auth/screens/register_screen.dart
+++ lib/auth/screens/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bongalo_recruitment/utils/validators.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -8,10 +9,32 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late TextEditingController _usernameController;
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _usernameController = TextEditingController();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form(
+        key: _formKey,
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
           child: ListView(
@@ -32,30 +55,23 @@ class _RegisterScreenState extends State<RegisterScreen> {
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: TextFormField(
+                  controller: _usernameController,
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    labelText: "First name",
+                    labelText: "Username",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(6),
                     ),
                   ),
+                  validator: (value) {
+                    return Validators.required("Username", value);
+                  },
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: TextFormField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    labelText: "Last name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: TextFormField(
+                  controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
@@ -69,6 +85,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: TextFormField(
+                  controller: _passwordController,
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                     labelText: "Password",
@@ -76,27 +93,36 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       borderRadius: BorderRadius.circular(6),
                     ),
                   ),
+                  validator: (value) {
+                    return Validators.validatePassword(
+                        value, _usernameController.text);
+                  },
                   obscureText: true,
                 ),
               ),
               SizedBox(
                 height: 20,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Container(
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(6),
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Register",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
+              GestureDetector(
+                onTap: () {
+                  _formKey.currentState!.validate();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Container(
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
--- test/auth/screens/register_screen_test.dart
+++ test/auth/screens/register_screen_test.dart
@@ -1,7 +1,86 @@
+import 'package:bongalo_recruitment/auth/screens/register_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('register screen ...', (tester) async {
-    // TODO: Implement test
+  testWidgets("The Username is required", (tester) async {
+    await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
+    Finder submit = find.widgetWithText(Container, "Register");
+    await tester.tap(submit);
+    await tester.pump();
+
+    expect(find.text("Username is required"), findsOneWidget);
+  });
+
+  testWidgets(
+      'Given a password, when it doesn\'t contain at least one uppercase letter then the validator will fails',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
+    final Finder password = find.widgetWithText(TextFormField, "Password");
+    final Finder submit = find.widgetWithText(Container, 'Register');
+    await tester.enterText(password, "secret");
+    await tester.tap(submit);
+    await tester.pump();
+    expect(find.text("One uppercase letter"), findsOneWidget);
+  });
+
+  testWidgets(
+      'Given a password with a Uppercase letter, when it doesn\'t contain at least one lowercase letter the validator will fails',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
+
+    final Finder password = find.widgetWithText(TextFormField, "Password");
+    final Finder submit = find.widgetWithText(Container, 'Register');
+    await tester.enterText(password, "SECRET");
+    await tester.tap(submit);
+    await tester.pump();
+
+    expect(find.text("One lowercase letter"), findsOneWidget);
+  });
+
+  testWidgets(
+      'Given a password with a Uppercase letter, lowecase letter, when the length <8 then the validator will fails',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
+
+    final Finder password = find.widgetWithText(TextFormField, "Password");
+    final Finder submit = find.widgetWithText(Container, 'Register');
+    await tester.enterText(password, "Secret");
+    await tester.tap(submit);
+    await tester.pump();
+
+    expect(
+        find.text("Minimum password length of 8 characters"), findsOneWidget);
+  });
+
+  testWidgets(
+      'Given a password of 8 character min with a Uppercase letter, lowecase letter, when it doesn\'t contain at 2 specials characters the validator will fails',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
+
+    final Finder password = find.widgetWithText(TextFormField, "Password");
+    final Finder submit = find.widgetWithText(Container, 'Register');
+    await tester.enterText(password, "Secretdepau");
+    await tester.tap(submit);
+    await tester.pump();
+
+    expect(find.text("Two special characters"), findsOneWidget);
+  });
+
+  testWidgets(
+      'Given a password of 8 character min with a Uppercase letter, lowecase letter and at least two specials characters when it  contain the username string the validator will fails',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
+    final Finder username = find.widgetWithText(TextFormField, "Username");
+    final Finder password = find.widgetWithText(TextFormField, "Password");
+    final Finder submit = find.widgetWithText(Container, 'Register');
+
+    await tester.enterText(username, "geek");
+    await tester.enterText(password, "Secretgeek@#");
+    await tester.tap(submit);
+    await tester.pump();
+
+    expect(
+        find.text("Password should not contain the username"), findsOneWidget);
   });
 }
